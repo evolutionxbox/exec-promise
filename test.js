a62453e51c3cdd89bb94874ad51fd7f92140298b
Jonathan Cousins <evolutionxbox@gmail.com>
2018-11-07 23:42:24 +0000

    test 2018-11-07-234224

---
+++ test.js
@@ -0,0 +1,38 @@
+console.clear()
+
+const { exec } = require('child_process')
+const log = require('npmlog')
+
+const sleep = delay => new Promise(fn => setTimeout(fn, delay))
+
+const execp = cmd => new Promise((s,f) => exec(cmd, (e, o, _) => e ? f(e) : s(o)))
+
+// exec('git status test.js', (a, b, c) => {
+//   log.notice('',b,c)
+// 
+//   exec('git status --short --branch .gitignore', (a, b, c) => {
+//     log.notice('',b,c)
+//   })
+// })
+
+// sleep(200)
+//   .then(_ => {
+//     log.notice('200')
+//     sleep(300)
+//       .then(_ => {
+//         log.notice('300')
+//       })
+//   })
+
+execp('git add test.js')
+  .then(_ => {
+    log.notice('add')
+    execp('git commit -m "test $(date +%F-%H%M%S)"')
+      .then(_ => {
+        log.notice('commit')
+        execp('git status')
+          .then(_ => {
+            log.notice('status')
+          })
+      })
+  })
